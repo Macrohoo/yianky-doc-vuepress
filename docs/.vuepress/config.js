8032e4c531643c01500853d85148a10ee0e5cd14
yian <105950987@qq.com>
2022-08-15 22:11:36 +0800

    feat: 增加百度网站统计

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,23 @@
 const { defaultTheme } = require('@vuepress/theme-default')
 
 module.exports = {
-  head: [['link', { rel: 'icon', href: '/images/icon256.png' }]],
+  head: [
+    ['link', { rel: 'icon', href: '/images/icon256.png' }],
+    [
+      // 添加百度统计代码
+      "script",
+      {},
+      `
+        var _hmt = _hmt || [];
+        (function() {
+          var hm = document.createElement("script");
+          hm.src = "https://hm.baidu.com/hm.js?56c08efe924f0aa2c23c2256632fdaa1";
+          var s = document.getElementsByTagName("script")[0];
+          s.parentNode.insertBefore(hm, s);
+        })();
+      `
+    ]
+  ],
   title: 'Yianky',  // 设置网站标题
   description : '一款次生代的浏览器',
   base : '/',
